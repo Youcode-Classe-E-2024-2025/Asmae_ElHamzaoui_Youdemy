68a54bb12600dab95a7a9934c5d4876b312bdf05
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-01-13 20:05:01 +0100

    création de la base de donnée courbase ainsi que la table user pour stocker les utilisateurs

---
+++ db.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE coursbase;
+
+use coursbase;
+
+CREATE TABLE user (
+    id_user INT AUTO_INCREMENT PRIMARY KEY,
+    user_name VARCHAR(255) NOT NULL,
+    user_photo BLOB,
+    user_email VARCHAR(255) UNIQUE NOT NULL,
+    user_password VARCHAR(255) NOT NULL,
+    user_role ENUM('Etudiant', 'Enseignant', 'Admin') NOT NULL
+);
