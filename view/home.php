630ef76c6d52f1a32f21daaa7b0842414a37190c
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-01-14 13:50:05 +0100

    implémentation du header dans un fichier séparé

--- view/home.php
+++ view/home.php
@@ -27,20 +27,9 @@
 <body>
 
     <!-- Header -->
-    <header class="fixed top-0 left-0 w-full bg-transparent py-2 px-6 bg-white">
-        <div class="flex items-center justify-between">
-            <img src="../assets/images/logo.png" alt="Logo" class="w-12 m-2">
-            <nav class="space-x-8 md:flex">
-                <a href="home.php" class="text-black font-bold hover:text-green-500">Home</a>
-                <a href="AboutUs.php" class="text-black font-bold hover:text-green-500">About us</a>
-                <a href="Cours.php" class="text-black font-bold hover:text-green-500">Cours</a>
-            </nav>
-            <div class="space-x-4">
-                <a href="direction.php" style="background-color:#1c4933" class="text-white px-4 py-2 border border-white font-bold rounded-md hover:bg-green-500">Sign Up</a>
-                <a href="connexion.php" style="background-color:#1c4933" class="text-white px-4 py-2 border border-white font-bold rounded-md hover:bg-green-500">Log In</a>
-            </div>
-        </div>
-    </header>
+   <?php 
+   include_once 'header.php';
+   ?>
 
     <!-- Section du canvas -->
     <section class="py-6 mt-2">
