888e4c922069f416e099bb87a7a94af379f12acc
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-01-14 20:00:34 +0100

    encapsulation de la classe user dans le fichier user.php

---
+++ model/user.php
@@ -0,0 +1,49 @@
+<?php
+require_once '../config/db.php';
+
+class user{
+     
+    //  propriétes de la classe user
+     private $id_user;
+     private $nom_user;
+     private $email_user;
+     private $passWord_user;
+     private $role_user;
+     
+     // constructeur de la classe user
+     public function __construct($id_user, $nom_user, $email_user, $passWord_user, $role_user){
+        $this->id_user = $id_user;
+        $this->nom_user = $nom_user;
+        $this->email_user = $email_user;
+        $this->passWord_user = $passWord_user;
+        $this->role_user = $role_user;
+     }
+    
+    //  getters pour la classe user
+    public function getIdUser(){ return $this->id_user ;}
+
+    public function getNomUser(){return $this->nom_user ;}
+
+    public function getEmailUser(){return $this->email_user ;}
+
+    public function getRoleUser(){return $this->role_user ;}
+
+   //  setters pour la classe user
+   public function setNomUser($nom_user){$this->nom_user = $nom_user;}
+
+   public function setEmailUser($email_user){$this->email_user = $email_user;}
+
+   public function setPasswordUser($passWord_user){$this->passWord_user = $passWord_user ;}
+
+   public function setRoleUser($role_user){$this->role_user = $role_user;}
+
+
+
+
+
+
+}
+
+
+
+?>
